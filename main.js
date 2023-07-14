e17bd9c475218447754d9ac42969e52c539359a8
Fatih Kabay <mfthkabay@gmail.com>
2023-07-14 21:42:06 +0300

    scene added is messi and space background image

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from "three";
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000 );
@@ -29,7 +31,35 @@ const ambianLight = new THREE.AmbientLight(0xffffff)
 scene.add( pointLight, ambianLight )
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-scene.add(lightHelper)
+const gridHelper = new THREE.GridHelper( 200, 50 )
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+  const star = new THREE.Mesh( geometry, material )
+ 
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ))
+  
+  star.position.set(x, y, z)
+  scene.add( star )
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+scene.background = spaceTexture
+
+const messiTexture = new THREE.TextureLoader().load('messi.jpg')
+
+const messi = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial( {map: messiTexture} )
+)
+
+scene.add(messi)
 
 
 function animate() {
@@ -38,6 +68,8 @@ function animate() {
   torus.rotation.x += 0.05;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  controls.update();
 
   renderer.render( scene, camera )
 }
