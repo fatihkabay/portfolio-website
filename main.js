fa176563b34cafaf46d336cc3617b0572b555fda
Fatih Kabay <mfthkabay@gmail.com>
2023-07-15 20:15:10 +0300

    add space and star

--- main.js
+++ main.js
@@ -1,77 +1,92 @@
-import './style.css'
+import "./style.css";
 
 import * as THREE from "three";
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg')
-})
+  canvas: document.querySelector("#bg"),
+});
 
-renderer.setPixelRatio( window.devicePixelRatio )
-renderer.setSize( window.innerWidth, window.innerHeight )
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render( scene, camera )
+renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } )
-const torus = new THREE.Mesh( geometry, material )
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus)
+scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set( 20,20,20 )
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
 
-const ambianLight = new THREE.AmbientLight(0xffffff)
-scene.add( pointLight, ambianLight )
+const ambianLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambianLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper( 200, 50 )
-scene.add(lightHelper, gridHelper)
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
-  const star = new THREE.Mesh( geometry, material )
- 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ))
-  
-  star.position.set(x, y, z)
-  scene.add( star )
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
 }
 
-Array(200).fill().forEach(addStar)
+Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg')
-scene.background = spaceTexture
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
 
-const messiTexture = new THREE.TextureLoader().load('messi.jpg')
+const messiTexture = new THREE.TextureLoader().load("messi.jpg");
 
 const messi = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
-  new THREE.MeshBasicMaterial( {map: messiTexture} )
-)
+  new THREE.MeshBasicMaterial({ map: messiTexture })
+);
+scene.add(messi);
 
-scene.add(messi)
+const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("normal.jpg");
 
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+);
+
+scene.add(moon);
 
 function animate() {
-  requestAnimationFrame( animate )
-  
+  requestAnimationFrame(animate);
+
   torus.rotation.x += 0.05;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
   controls.update();
 
-  renderer.render( scene, camera )
+  renderer.render(scene, camera);
 }
 
 animate();
